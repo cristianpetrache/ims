dbdbd48458f4989712c7eb8de475782f6aa3e413
Adrian <neacsu.adrian96@gmail.com>
2017-07-18 15:31:40 +0300

    added configurable database login credentials from properties file

--- src/main/java/ConnectionJDBC.java
+++ src/main/java/ConnectionJDBC.java
@@ -1,50 +1,60 @@
-/**
- * Created by User on 7/14/2017.
- */
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-
+/**
+ * Created by User on 7/10/2017.
+ */
 public class ConnectionJDBC {
-
-    Properties prop = new Properties();
-    InputStream input = null;
-
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/users";
-    public static final String JDBC_USER = "root";
-    public static final String JDBC_PASS = "root";
-
-
+    public static String JDBC_URL;
+    public static String JDBC_USER;
+    public static String JDBC_PASS;
 
     static {
+        Properties properties = new Properties();
+        InputStream inputStream = null;
+
         try {
+            //inputStream = new FileInputStream("db.properties");
+            properties.load(ConnectionJDBC.class.getResourceAsStream("/database.properties"));
+
+            JDBC_URL=properties.getProperty("JDBC_URL");
+            JDBC_USER=properties.getProperty("JDBC_USER");
+            JDBC_PASS=properties.getProperty("JDBC_PASS");
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if( inputStream!=null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public static Connection getConection() {
         Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
-        return connection;
-    }
 
-    public static void close(AutoCloseable closeable) {
-        if (closeable == null) {
-            return;
-        }
         try {
-            closeable.close();
-        } catch (Exception e){
+            connection= DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASS);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        return connection;
     }
 }
